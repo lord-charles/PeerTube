99decb8907b4b2606fb9a358faa49f7a46e816f7
lord-charles <mwanikicharles226@gmail.com>
2025-06-21 18:40:00 +0300

    edits

--- client/src/app/+about/about.component.ts
+++ client/src/app/+about/about.component.ts
@@ -1,62 +1,67 @@
-import { CommonModule } from '@angular/common'
-import { Component, OnInit, inject, viewChild } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
-import { ServerService } from '@app/core'
-import { GlobalIconComponent } from '@app/shared/shared-icons/global-icon.component'
-import { Actor } from '@app/shared/shared-main/account/actor.model'
-import { ButtonComponent } from '@app/shared/shared-main/buttons/button.component'
-import { HorizontalMenuComponent, HorizontalMenuEntry } from '@app/shared/shared-main/menu/horizontal-menu.component'
-import { SupportModalComponent } from '@app/shared/shared-support-modal/support-modal.component'
-import { maxBy } from '@peertube/peertube-core-utils'
-import { HTMLServerConfig } from '@peertube/peertube-models'
+import { CommonModule } from "@angular/common";
+import { Component, OnInit, inject, viewChild } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ServerService } from "@app/core";
+import { GlobalIconComponent } from "@app/shared/shared-icons/global-icon.component";
+import { Actor } from "@app/shared/shared-main/account/actor.model";
+import { ButtonComponent } from "@app/shared/shared-main/buttons/button.component";
+import {
+  HorizontalMenuComponent,
+  HorizontalMenuEntry,
+} from "@app/shared/shared-main/menu/horizontal-menu.component";
+import { SupportModalComponent } from "@app/shared/shared-support-modal/support-modal.component";
+import { maxBy } from "@peertube/peertube-core-utils";
+import { HTMLServerConfig } from "@peertube/peertube-models";
 
 @Component({
-  selector: 'my-about',
-  templateUrl: './about.component.html',
-  styleUrls: [ './about.component.scss' ],
-  imports: [ CommonModule, RouterOutlet, HorizontalMenuComponent, GlobalIconComponent, ButtonComponent ]
+  selector: "my-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HorizontalMenuComponent,
+    GlobalIconComponent,
+    ButtonComponent,
+  ],
 })
 export class AboutComponent implements OnInit {
-  private server = inject(ServerService)
+  private server = inject(ServerService);
 
-  readonly supportModal = viewChild<SupportModalComponent>('supportModal')
+  readonly supportModal = viewChild<SupportModalComponent>("supportModal");
 
-  bannerUrl: string
-  avatarUrl: string
+  bannerUrl: string;
+  avatarUrl: string;
 
-  menuEntries: HorizontalMenuEntry[] = []
+  menuEntries: HorizontalMenuEntry[] = [];
 
-  config: HTMLServerConfig
+  config: HTMLServerConfig;
 
-  ngOnInit () {
-    this.config = this.server.getHTMLConfig()
+  ngOnInit() {
+    this.config = this.server.getHTMLConfig();
 
-    this.bannerUrl = this.config.instance.banners.length !== 0
-      ? maxBy(this.config.instance.banners, 'width').path
-      : undefined
+    this.bannerUrl =
+      this.config.instance.banners.length !== 0
+        ? maxBy(this.config.instance.banners, "width").path
+        : undefined;
 
-    this.avatarUrl = Actor.GET_ACTOR_AVATAR_URL(this.config.instance, 110)
+    this.avatarUrl = Actor.GET_ACTOR_AVATAR_URL(this.config.instance, 110);
 
     this.menuEntries = [
       {
         label: $localize`Platform`,
-        routerLink: '/about/instance',
-        pluginSelectorId: 'about-menu-instance'
-      },
-      {
-        label: $localize`PeerTube`,
-        routerLink: '/about/peertube',
-        pluginSelectorId: 'about-menu-peertube'
+        routerLink: "/about/instance",
+        pluginSelectorId: "about-menu-instance",
       },
       {
         label: $localize`Network`,
-        routerLink: '/about/follows',
-        pluginSelectorId: 'about-menu-network'
-      }
-    ]
+        routerLink: "/about/follows",
+        pluginSelectorId: "about-menu-network",
+      },
+    ];
   }
 
-  isContactFormEnabled () {
-    return this.config.email.enabled && this.config.contactForm.enabled
+  isContactFormEnabled() {
+    return this.config.email.enabled && this.config.contactForm.enabled;
   }
 }
